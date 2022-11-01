1fc8973b01cea9e62494f33dd7c2915be8dd291e
Andrey Vlasenko <andrewvenko95@gmail.com>
2022-11-01 18:29:35 +0300

    Error correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  // Вычисление дескриминанта
-  let d = Math.pow(b,2) - 4 * a * c;
+  const arr = [];
+  // Вычисление дискриминанта
+  const discriminant = Math.pow(b,2) - 4 * a * c;
   // Проверка дискриминанта и нахождение корней
-  if (d > 0) {
-    arr[0] = Math.trunc(((-b + Math.sqrt(d)) / (2 * a)) * 100) / 100;
-    arr[1] = Math.trunc(((-b - Math.sqrt(d)) / (2 * a)) * 100) / 100;
-  } else if (d === 0) {
-    arr[0] = Math.trunc((-b / (2 * a)) * 100) / 100;
+  if (discriminant > 0) {
+    arr[0] = +((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(2);
+    arr[1] = +((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(2);
+  } else if (discriminant === 0) {
+    arr[0] = +(-b / (2 * a)).toFixed(2);
   };
   return arr; // array
 };
@@ -17,30 +17,30 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount = 0;
   // Проверка на соотвествие числовому формату
   if (Number.isNaN(parseInt(percent))) {
-    alert('Параметр \"percent\" содержит неправильные значения ' + `${percent}`);
+    return totalAmount = 'Параметр \"percent\" содержит неправильные значения ' + `${percent}`;
   } else {
     percent = parseInt(percent);
   };
   if (Number.isNaN(parseInt(contribution))) {
-    alert('Параметр \"contribution\" содержит неправильные значения ' + `${contribution}`);
+    return totalAmount = 'Параметр \"contribution\" содержит неправильные значения ' + `${contribution}`;
   } else {
     contribution = parseInt(contribution);
   };
   if (Number.isNaN(parseInt(amount))) {
-    alert('Параметр \"amount\" содержит неправильные значения ' + `${amount}`);
+    return totalAmount = 'Параметр \"amount\" содержит неправильные значения ' + `${amount}`;
   } else {
     amount = parseInt(amount);
   };
   // Расчет тела кредита и срока в месяцах
-  let bodyCredit = amount - contribution;
-  let dateTo = new Date(2022, 9, 31);
-  let amountMonths = date.getMonth() - dateTo.getMonth() + (12 * (date.getFullYear() - dateTo.getFullYear()));
+  const bodyCredit = amount - contribution;
+  const dateTo = new Date();
+  const amountMonths = date.getMonth() - dateTo.getMonth() + (12 * (date.getFullYear() - dateTo.getFullYear()));
   // Расчет платежей и общей суммы
-  let percentBid = (percent / 100) / 12;
+  const percentBid = (percent / 100) / 12;
   for (let i = 0; i < amountMonths; i = i + 1) {
     let payment = bodyCredit * (percentBid + (percentBid / ((Math.pow((1  + percentBid),  amountMonths)) - 1)));
     totalAmount = totalAmount + payment;
   };
-  totalAmount = Math.trunc(totalAmount * 100) / 100;
+  totalAmount = +totalAmount.toFixed(2);
   return totalAmount;
 };
