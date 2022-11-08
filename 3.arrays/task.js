f0d622df9954871dc180cac56affe5b90b6d406b
Andrey Vlasenko <andrewvenko95@gmail.com>
2022-11-08 14:29:13 +0300

    Change 3.arrays/task/js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,31 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length !== arr2.length) {
+    return result = false;
+  } else if (arr1.length === arr2.length) {
+    let resultComparison = getComparison(arr1, arr2);
+    if (resultComparison === false) {
+      return result = false;
+    } else {
+      result = true;
+    };
+  };
   return result; // boolean
 }
 
+function getComparison(arr1, arr2) {
+  let resultComparison;
+  for (let i = 0; i < arr1.length; i = i + 1) {
+    if (arr1[i] !== arr2[i]) {
+        return resultComparison = false;
+    } else if (arr1[i] === arr2[i]) {
+      resultComparison = true;
+    };
+  };
+  return resultComparison;
+};
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(element => element % 3 === 0).filter(element => element > 0).map(element => element * 10);
   return resultArr; // array
 }
