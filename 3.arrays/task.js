840f5720cfc9916ca1cd4bf3771475a87ce20388
Andrey Vlasenko <andrewvenko95@gmail.com>
2022-11-14 19:44:30 +0300

    Change 4.objects/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,6 @@
 function compareArrays(arr1, arr2) {
   let result;
-  if (arr1.length === arr2.length) {
-    result = arr1.every((element, index) => element === arr2[index]);
-  } else {
-    result = false;
-  };
-  return result; // boolean
+  return result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]); // boolean
 };
 
 function advancedFilter(arr) {
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+};
 
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  };
+};
 
-// ваш код для остальных методов
+Student.prototype.marks = function addMarks(mark1, mark2, mark3, ...mark) {
+  this.marks.push(mark1, mark2, mark3, ...mark);
+};
+
+Student.prototype.getAverage = function() {
+  let arithteticMean = 0;
+  let arr;
+  for (let value of Object.entries(this)) {
+    if (value[0] === 'marks') {
+      arr = value[1];
+    }
+  };
+  for(let i = 0; i < arr.length; i = i + 1) {
+    arithteticMean = arithteticMean + arr[i];
+  };
+  arithteticMean = arithteticMean / arr.length;
+  this.arithteticMean = arithteticMean;
+};
+
+Student.prototype.exclude = function exclude(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
