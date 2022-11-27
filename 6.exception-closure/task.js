dd7012bf197b48729983f82a415ce30147593587
Andrey Vlasenko <andrewvenko95@gmail.com>
2022-11-27 22:30:52 +0300

    Changes made in 6.exception-closure/task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -4,23 +4,21 @@ function parseCount(value){
     const parsed = parseInt(value, 10);
     if(Number.isNaN(parsed)){
         throw new Error('Ошибка, невалидное значение числа');
-    } else{
-        return parsed;
     };
+    return parsed;
 };
-let errorName = new Error('Ошибка!');
 function validateCount(value){
     try{
         return parseCount(value);
-    } catch(errorName){
-        console.log(errorName);
+    } catch(error){
+        return error;
     };
 };
 
 //Задача №2
 class Triangle{
     constructor(a,b,c){
-        if((a + b > c) || (a + c > b) || (b + c > a)){
+        if((a + b < c) || (a + c < b) || (b + c < a)){
             throw new Error('Треугольник с такими сторонами не существует')
         };
         this.a = a;
@@ -29,21 +27,24 @@ class Triangle{
     };
     //  Методы
     getPerimeter(){
-        return this.P = this.a + this.b + this.c;
+        return (this.a + this.b + this.c);
     };
     getArea(){
-        let p = this.P / 2;
-        return this.S = Math.pow((p(p - this.a)(p - this.b)(p - this.c)), 0.5); 
+        const p = this.getPerimeter() / 2;
+        return (Math.pow((p * (p - this.a) * (p - this.b) * (p - this.c)), 0.5)); 
     };
 };
 function getTriangle(a,b,c){
     try{
-        console.log('Внутри блока try');
         return new Triangle(a,b,c);
     } catch(errorName){
-        console.log('Внутри блока catch');
-        return getPerimeter(), getArea(), new Error('Ошибка! Треугольник не существует');
-    } finally{
-        console.log('Внутри блока finally');
+        return {
+            getPerimeter(){
+                return new Error('Ошибка! Треугольник не существует');
+            },
+            getArea(){
+                return new Error('Ошибка! Треугольник не существует');
+            },
+        };
     };
 };
